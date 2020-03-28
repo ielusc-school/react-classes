dabfbaadd797a29f6e8e327133862ea2957f235a
Cristofer Sousa <cristofer.sousa@gmail.com>
2020-03-28 03:26:16 -0300

    added files and folders

---
+++ React/aula6/calculator/src/main/Calculator.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+// como iremos usar estados vamos ter que usar class....
+
+export default class Calculator extends Component {
+  render () {
+    return (
+      <div className="calculator"></div>
+    )
+  }
+}
