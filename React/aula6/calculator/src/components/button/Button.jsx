7c575c9aba0eb05df945c87717b363e81cd9aa1d
Cristofer Sousa <cristofer.sousa@gmail.com>
2020-03-28 04:12:34 -0300

    added buttons to calculator with props

---
+++ React/aula6/calculator/src/components/button/Button.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Button.css'
+
+// components sem estado (stateless)
+
+export default props => <button className='button'>{ props.label }</button>
--- React/aula6/calculator/src/main/Calculator.jsx
+++ React/aula6/calculator/src/main/Calculator.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import './Calculator.css';
 // como iremos usar estados vamos ter que usar class....
+import Button from '../components/button/Button';
 
 export default class Calculator extends Component {
   render () {
     return (
-      <div className="calculator"></div>
+      <div className="calculator">
+        <Button label="AC"/>
+        <Button label="/"/>
+        <Button label="7"/>
+        <Button label="8"/>
+        <Button label="9"/>
+        <Button label="X"/>
+        <Button label="4"/>
+        <Button label="5"/>
+        <Button label="6"/>
+        <Button label="-"/>
+        <Button label="1"/>
+        <Button label="2"/>
+        <Button label="3"/>
+        <Button label="+"/>
+        <Button label="0"/>
+        <Button label="."/>
+        <Button label="="/>
+      </div>
     )
   }
 }
